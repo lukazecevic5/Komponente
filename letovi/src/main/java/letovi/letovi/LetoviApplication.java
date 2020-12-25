17bedc43573fcbe54fd4fbfc1457a1aa032fc141
Luka <Luka@DESKTOP-MJ8AL4V>
2020-12-25 23:19:21 +0100

    first

---
+++ letovi/src/main/java/letovi/letovi/LetoviApplication.java
@@ -0,0 +1,13 @@
+package letovi.letovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetoviApplication.class, args);
+	}
+
+}
---
+++ letovi/src/test/java/letovi/letovi/LetoviApplicationTests.java
@@ -0,0 +1,13 @@
+package letovi.letovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
