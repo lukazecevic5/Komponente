00dd0e8133e383337635087a78bcd92b40965c41
klini <klini@DESKTOP-CQHEEHC>
2021-01-05 15:07:35 +0100

    Eureka i zuul diplojovani.

--- karte/src/main/java/app/KarteApplication.java
+++ karte/src/main/java/app/KarteApplication.java
@@ -1,9 +1,11 @@
 package app;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class KarteApplication {
 
 	public static void main(String[] args) {
