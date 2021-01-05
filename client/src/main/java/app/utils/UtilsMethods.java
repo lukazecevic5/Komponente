37cc681cd4e796fa6902ab687c0407d01b91521e
klini <klini@DESKTOP-CQHEEHC>
2021-01-05 20:07:59 +0100

    utils metode

---
+++ client/src/main/java/app/utils/UtilsMethods.java
@@ -0,0 +1,123 @@
+package app.utils;
+
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class UtilsMethods {
+
+	
+	public static ResponseEntity<String> sendGetStr(String url) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+		return response;
+	}
+
+	public static ResponseEntity<String> sendPostStr(String url, Object body) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<Object> entity = new HttpEntity<Object>(body, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+		return response;
+	}
+	
+	public static ResponseEntity<Integer> sendGetInt(String url) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<Integer> response = restTemplate.exchange(url, HttpMethod.GET, entity, Integer.class);
+
+		return response;
+	}
+
+	public static ResponseEntity<Integer> sendPostInt(String url, Object body) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<Object> entity = new HttpEntity<Object>(body, headers);
+
+		ResponseEntity<Integer> response = restTemplate.exchange(url, HttpMethod.POST, entity, Integer.class);
+
+		return response;
+	}
+	
+	public static ResponseEntity<Long> sendGetLg(String url) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<Long> response = restTemplate.exchange(url, HttpMethod.GET, entity, Long.class);
+
+		return response;
+	}
+
+	public static ResponseEntity<Long> sendPostLg(String url, Object body) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<Object> entity = new HttpEntity<Object>(body, headers);
+
+		ResponseEntity<Long> response = restTemplate.exchange(url, HttpMethod.POST, entity, Long.class);
+
+		return response;
+	}
+	
+	public static ResponseEntity<List<Long>> sendGetLs(String url) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<List<Long>> response = restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<Long>>() {});
+
+		return response;
+	}
+	
+	public static ResponseEntity<Boolean> sendGetBool(String url) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<Boolean> response = restTemplate.exchange(url, HttpMethod.GET, entity, Boolean.class);
+
+		return response;
+	}
+	
+	public static ResponseEntity<Float> sendGetFloat(String url) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<Float> response = restTemplate.exchange(url, HttpMethod.GET, entity, Float.class);
+
+		return response;
+	}
+	
+}
